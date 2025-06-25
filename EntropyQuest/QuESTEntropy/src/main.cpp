f6b72a9cd61830bd6054ab2950a1d75aac52d682
maricix-18 <mariabahna1808@gmail.com>
2025-06-25 13:52:49 +0000

    data gathering purity and R2d for Q13-Q16 depth = 15

--- EntropyQuest/QuESTEntropy/src/main.cpp
+++ EntropyQuest/QuESTEntropy/src/main.cpp
@@ -118,13 +118,14 @@ int main()
   int depth_max = 15;
 
   // set qubits
-  int qubits_max = 15;
+  int qubits_max = 17;
 
   // angles
   int angles_per_layer = 2*qubits_max;
   int total_angles = angles_per_layer*depth_max;
   double angles_array[total_angles];
 
+  printf("Generating angles.\n");
   // get angle values
   for (int i = 0; i < total_angles; i++) {
     double r = 2*PI*genrand_res53();
@@ -143,7 +144,7 @@ int main()
   float p2 = 0.054;
   //p2 = (15 * p2 / 16.0); // scale for quest function
 
-  for (int qubits = 11; qubits <= qubits_max; qubits ++)
+  for (int qubits = 13; qubits <= qubits_max; qubits ++)
   {
     // time round
     auto start = high_resolution_clock::now(); 
@@ -153,14 +154,10 @@ int main()
     // loop for different qubit sizes
     int dim = pow(2, qubits);
 
-    // data gathering
-    // vector<double> vNd;
-    // vector<double> pur;
-    // vector<double> R2d;
+    cout << "Qubits: "<< qubits << " Depth: " << depth_max <<endl;
 
-    cout<< "Qubits: "<<qubits<< " Depth: " <<depth_max <<endl;
     // create env and dens mat qreg
-    printf("Create Q Register\n");
+    cout << "Create Q Register" << endl;
     Qureg density_matrix_qreg = createDensityQureg(qubits, env);
 
     // populate circuit
@@ -169,14 +166,13 @@ int main()
     for (int i = 0; i <= depth_max; i++)
     {
       // time each depth
-      //auto start = high_resolution_clock::now(); 
+      auto start = high_resolution_clock::now(); 
       printf("layer %d \n", i);
       // for each layer add x rotation on all qubits
       for (int j = 0; j < qubits; j++)
       {
         //printf("rx gate: Angle = %.15lf, Applied to q[%d]\n", angles_array[angl_pos], j);
         rotateX(density_matrix_qreg, j, angles_array[angl_pos]);
-        //mixDepolarising(density_matrix_qreg, j, p1);
         densmatr_mixDepolarising(density_matrix_qreg, j, p1);
         angl_pos++;
       }
@@ -186,7 +182,6 @@ int main()
       {
         //printf("ry gate: Angle = %.15lf, Applied to q[%d]\n", angles_array[angl_pos], j);
         rotateY(density_matrix_qreg, j, angles_array[angl_pos]);
-        //mixDepolarising(density_matrix_qreg, j, p1);
         densmatr_mixDepolarising(density_matrix_qreg, j, p1);
         angl_pos++;
       }
@@ -198,13 +193,14 @@ int main()
         {
           //printf("Cz gate: Applied to control q[%d], and target q[%d]\n", j, j+1);
           controlledPhaseFlip(density_matrix_qreg, j, j+1);
-          //mixTwoQubitDepolarising(density_matrix_qreg, j, j+1,p2);
           densmatr_mixTwoQubitDepolarising(density_matrix_qreg, j, j+1,p2);
         }
       }
 
       // compute metrics
+      cout<< "Get eingenmat for layer " << i << endl;
       // get matrix to use Eingensolver library
+      #pragma omp parallel for
       Eigen::MatrixXcd eing_mat(dim, dim);
       for (int i = 0; i < dim; i++)
       {
@@ -212,37 +208,31 @@ int main()
         {
           Complex amplitude = getDensityAmp(density_matrix_qreg, i, j);
           eing_mat(i,j) = C(amplitude.real, amplitude.imag);
-          //eing_mat(i,j) = C(real, imag);
-          //printf("%+.8f%+.8fi ", amplitude.real, amplitude.imag);
-          // cout << eing_mat(i,j).real();
         }
-        //printf("\n");
       }
     
-      // get eingenvalues
-      Eigen::ComplexEigenSolver<Eigen::MatrixXcd> ces;
-      ces.compute(eing_mat);
-      //cout<< "Eingenvalues of matrix are: " << ces.eigenvalues()(0).real();
-      complex<double> sum_entropy = 0 ;
-      complex<double> sum_eingvals = 0;
-      for (int e = 0; e < dim; e++)
-      {
-        //calculate ln
-        complex<double> log_z = log(abs(ces.eigenvalues()(e))) + 1i * arg(ces.eigenvalues()(e));
-        complex<double> log2_z = log_z / log(2.0);
-        sum_entropy += (-ces.eigenvalues()(e) * log2_z); 
-        sum_eingvals += ces.eigenvalues()(e);
-      }
+      /** The matrix is first reduced to Schur form using the ComplexSchur class. 
+      /*  The Schur decomposition is then used to compute the eigenvalues and eigenvectors.
+      /*  The cost of the computation is dominated by the cost of the Schur decomposition, which is O(n^3) where n is the size of the matrix.
+      */
+      // // get eingenvalues
+      // Eigen::ComplexEigenSolver<Eigen::MatrixXcd> ces;
+      // cout<< "Compute eingenvalues for layer " << i << endl;
+      // ces.compute(eing_mat, false); // compute only eingenvalues, not eigenvectors 
+      // cout<< "Eingenvalues of matrix are: " << ces.eigenvalues()(0).real();
+      // complex<double> sum_entropy = 0 ;
+      // complex<double> sum_eingvals = 0;
+      // for (int e = 0; e < dim; e++)
+      // {
+      //   //calculate ln
+      //   complex<double> log_z = log(abs(ces.eigenvalues()(e))) + 1i * arg(ces.eigenvalues()(e));
+      //   complex<double> log2_z = log_z / log(2.0);
+      //   sum_entropy += (-ces.eigenvalues()(e) * log2_z); 
+      //   sum_eingvals += ces.eigenvalues()(e);
+      // }
       
-      // for each layer
-      // Von neumann entropy
-      // vNd.push_back(abs(sum_entropy/double(qubits)));
-      // // Purity - the trace of density_matrix^2
-      // pur.push_back((eing_mat*eing_mat).trace().real());
-      // // R2 entropy
-      // R2d.push_back(-1 * log2(pur[i]) / qubits);
-
-      double vNd = abs(sum_entropy/double(qubits));
+      cout<<"Append to json for layer " << i << endl;
+      double vNd = 0;//abs(sum_entropy/double(qubits));  // don't calculate the vonneumann entropy
       double pur = (eing_mat*eing_mat).trace().real();
       double R2d = -1 * log2(pur) / qubits;
 
@@ -251,27 +241,13 @@ int main()
     }
     auto stop = high_resolution_clock::now();
 	  auto duration = duration_cast<nanoseconds>(stop - start).count();
-    // SAVE to json file
-    // string filename = "../../QuESTEntropy/DensityMatrices_metrics/Q" + std::to_string(qubits) + "_D15.json";
-    // ofstream file(filename);
-    // json j;
-    // j["all_vNd_diff_n"] = {vNd};
-    // j["all_pur_diff_n"] = {pur};
-    // j["all_R2d_diff_n"] = {R2d};
-    // j["duration"] = duration;
-
-    // if (file.is_open())
-    // {
-    //   file<<std::setw(4) << j; 
-    // }
+   
     append_to_json_duration(filename, duration);
     
-    //file.close();
     //destroy Quest register and environment
     printf("Destroy Qreg\n");
     destroyQureg(density_matrix_qreg, env);
 
-    //printf("%d\n ", angl_pos);
   }
 
   printf("Destroy Environment\n");
